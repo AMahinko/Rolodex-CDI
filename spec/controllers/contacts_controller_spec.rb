10750e4162fe30b97d82fd7b9a084965a8e07782
Alex <alex.mahinko@gmail.com>
2017-07-28 03:10:55 -0400

    wrote more robust format validation unit tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -3,15 +3,23 @@ require 'rails_helper'
 RSpec.describe ContactsController, type: :controller do
 
   it 'Capitalizes names' do
-    user = User.create({email: 'test@test.com', password: '1234', password_confirmation: '1234'})
-    contact = Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id})
+    user = User.create({email: 'test@tester.com', password: '1234', password_confirmation: '1234'})
+    contact = Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id, email: 'something@orother.com'})
     contact.save!
-    print Contact.all.inspect
     expect(contact.first_name).to eq('Lower')
     expect(contact.last_name).to eq('Case')
+    Contact.delete(1)
   end
 
   it 'validates email formatting' do
+    #only one is valid
+    user = User.create({email: 'test@tester.com', password: '1234', password_confirmation: '1234'})
+    Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id, email: 'somethingorother.com'})
+    Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id, email: 'something@orothercom'})
+    Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id, email: 'orother.com'})
+    Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id, email: 'some\$%^$g@oro\#ther.com'})
+    Contact.create({first_name: 'lower', last_name: 'case', user_id: user.id, email: 'something@orother.com'})
+    expect(Contact.all.count).to eq(1)
 
   end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,16 +4,27 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   it 'Checks for a unique email' do
-    User.create({email: 'test@tester.com', password: '1234', password_confirmation: '1234'})
-    User.create({email: 'test@tester.com', password: '1234', password_confirmation: '1234'})
+    User.create({email: 'unique@tester.com', password: '1234', password_confirmation: '1234'})
+    User.create({email: 'unique@tester.com', password: '1234', password_confirmation: '1234'})
     print User.all.inspect
     expect(User.count).to eq(1)
   end
 
   it 'Confirms your password' do
     print User.all.inspect
-    User.create({email: 'test@tester.com', password: '1234', password_confirmation: '123'})
+    User.create({email: 'wrong@tester.com', password: '1234', password_confirmation: '123'})
     expect(User.count).to eq(0)
+  end
+
+  it 'validates user email format' do
+    #only one is valid
+    User.create({email: 'testtester.com', password: '1234', password_confirmation: '1234'})
+    User.create({email: 'test@testercom', password: '1234', password_confirmation: '1234'})
+    User.create({email: 'tester.com', password: '1234', password_confirmation: '1234'})
+    User.create({email: 'te st@testercom', password: '1234', password_confirmation: '1234'})
+    User.create({email: 'test@tester.com', password: '1234', password_confirmation: '1234'})
+    print User.all.inspect
+    expect(User.all.count).to eq(1)
   end
 
 end
